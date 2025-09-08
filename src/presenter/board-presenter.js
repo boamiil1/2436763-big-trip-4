ed7d6a59600eae474d0ec55fc9393b317c7c3af4
Илья Шубин <starkovairina072@gmail.com>
2025-09-08 19:40:23 +0500

    add summary

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,27 +1,74 @@
-import SortView from '../view/sort-view.js';
-import EventListView from '../view/event-list-view.js';
-import { render } from '../render.js';
-import PointView from '../view/point-view.js';
-import PointEditView from '../view/point-edit-view.js';
-
-const POINT_COUNT = 3;
+import SortingView from '../view/sorting-view.js';
+import { render, RenderPosition} from '../framework/render.js';
+import TripPointListView from '../view/trip-point-list-view.js';
+import NoTripPointView from '../view/no-trip-point-view.js';
+import TripPointPresenter from './trip-point-presenter.js';
+import { updateItem } from '../utils.js';
 
 export default class BoardPresenter {
-  sortComponent = new SortView();
-  eventListComponent = new EventListView();
+  #tripContainer = null;
+  #tripPointsModel = null;
+  #tripPoints = null;
 
-  constructor({container}) {
-    this.container = container;
+  #tripPointsList = new TripPointListView();
+  #sortComponent = new SortingView();
+  #noTripPointComponent = new NoTripPointView();
+
+  #tripPointPresenter = new Map();
+
+  constructor(tripContainer, tripPointsModel) {
+    this.#tripContainer = tripContainer;
+    this.#tripPointsModel = tripPointsModel;
   }
 
-  init() {
-    render(this.sortComponent, this.container);
-    render(this.eventListComponent, this.container);
-    render(new PointEditView(), this.eventListComponent.getElement());
+  init () {
+    this.#tripPoints = [...this.#tripPointsModel.tripPoints];
 
-    for (let i = 0; i < POINT_COUNT; i++) {
-      render(new PointView(), this.eventListComponent.getElement());
-
+    if (this.#tripPoints.length === 0) {
+      this.#renderNoTripPoints();
+    }
+    else {
+      this.#renderSort();
+      this.#renderTripPointList();
     }
   }
+
+  #handleModeChange = () => {
+    this.#tripPointPresenter.forEach((presenter) => presenter.resetView());
+  };
+
+  #handlePointChange = (updatedPoint) => {
+    this.#tripPoints = updateItem(this.#tripPoints, updatedPoint);
+    this.#tripPointPresenter.get(updatedPoint.id).init(updatedPoint);
+  };
+
+  #renderSort = () => {
+    render(this.#sortComponent, this.#tripContainer, RenderPosition.AFTERBEGIN);
+  };
+
+  #renderTripPoint = (tripPoint) => {
+    const tripPointPresenter = new TripPointPresenter(this.#tripPointsList.element, this.#tripPointsModel, this.#handlePointChange, this.#handleModeChange);
+    tripPointPresenter.init(tripPoint);
+    this.#tripPointPresenter.set(tripPoint.id, tripPointPresenter);
+  };
+
+  #renderTripPoints = (from, to) => {
+    this.#tripPoints
+      .slice(from, to)
+      .forEach((tripPoint) => this.#renderTripPoint(tripPoint));
+  };
+
+  #renderNoTripPoints = () => {
+    render(this.#noTripPointComponent, this.#tripContainer, RenderPosition.AFTERBEGIN);
+  };
+
+  #clearTripPointList = () => {
+    this.#tripPointPresenter.forEach((presenter) => presenter.destroy());
+    this.#tripPointPresenter.clear();
+  };
+
+  #renderTripPointList = () => {
+    render(this.#tripPointsList, this.#tripContainer);
+    this.#renderTripPoints(0, this.#tripPoints.length);
+  };
 }
